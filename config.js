4f4710086b9d4d876d6b6c938bc1610e86d32b72
Poonam-Lunawat <68240432+Poonam-Lunawat@users.noreply.github.com>
2022-01-17 13:14:13 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAtZ5J8wSvde7b96Mj32kUtnoTrNBx2xt4",
+  authDomain: "storytelling-app-8a9e1.firebaseapp.com",
+  databaseURL: "https://storytelling-app-8a9e1-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-8a9e1",
+  storageBucket: "storytelling-app-8a9e1.appspot.com",
+  messagingSenderId: "670375710461",
+  appId: "1:670375710461:web:3cf5118e3cf0e7ee007894"
+};
